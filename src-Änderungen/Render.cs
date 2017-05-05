9211bc6361d4b33d8e8e2e478022d4bcfc0afbee
Simon Nestrowicz <simon.nestrowicz@gmx.de>
2017-05-05 15:26:29 +0000

    Upload new file

---
+++ src-Änderungen/Render.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ReiseZumGrundDesSees
+{
+	static class Render
+	{
+		public static void World(Matrix m, World _world)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static void Player(Matrix m, Player _player)
+		{
+            foreach (ModelMesh mesh in _player.model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+
+                    effect.World = Matrix.CreateTranslation(_player.pos);
+
+                    effect.View = m;
+
+                    effect.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), 800f / 480f, 0.1f, 5000f);
+
+                }
+
+                mesh.Draw();
+            }
+        }
+    }
+
+		// ...
+	}
+
